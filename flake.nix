5decc7d28f7321b247c182159e5d8f3d87ce920e
Sacha Morard <2254275+SachaMorard@users.noreply.github.com>
2024-05-02 11:17:59 +0200

    feat: install the first lines of code for the proxy (#1)
    * feat: install the first lines of code for the proxy
    
    * chore: cli mod

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Edgee";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ rustup libiconv ];
+        };
+      });
+}
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+#![deny(warnings)]
+
+mod config;
+mod cli;
+
+use bytes::Bytes;
+use http_body_util::{combinators::BoxBody, BodyExt};
+use hyper::client::conn::http1::Builder;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = cli::get_cli();
+    match &cli.command {
+        cli::Commands::Start { port, file } => {
+
+            // configuration
+            let conf = match config::configure(file) {
+                Ok(conf) => conf,
+                Err(_) => unreachable!(),
+            };
+
+            println!("File: {:?}", conf);
+
+            let addr = SocketAddr::from(([127, 0, 0, 1], *port));
+
+            let listener = TcpListener::bind(addr).await?;
+            println!("Listening on http://{}", addr);
+
+            // We start a loop to continuously accept incoming connections
+            loop {
+                let (stream, _) = listener.accept().await?;
+                let io = TokioIo::new(stream);
+
+                tokio::task::spawn(async move {
+                    if let Err(err) = http1::Builder::new()
+                        .preserve_header_case(true)
+                        .title_case_headers(true)
+                        .serve_connection(io, service_fn(proxy))
+                        .with_upgrades()
+                        .await
+                    {
+                        println!("Failed to serve connection: {:?}", err);
+                    }
+                });
+            }
+        }
+        _ => unreachable!(),
+    }
+}
+
+async fn proxy(req: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    println!("req: {:?} {:?}", req.method(), req.uri());
+
+    let host = "localhost";
+    let port = req.uri().port_u16().unwrap_or(3000);
+
+    let stream = TcpStream::connect((host, port)).await.unwrap();
+    let io = TokioIo::new(stream);
+
+    let (mut sender, conn) = Builder::new()
+        .preserve_header_case(true)
+        .title_case_headers(true)
+        .handshake(io)
+        .await?;
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
+
+    let resp = sender.send_request(req).await?;
+    Ok(resp.map(|b| b.boxed()))
+}
