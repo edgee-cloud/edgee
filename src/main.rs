6ad94ee15d60eb0cf5cf1c27602489c10ef16804
Alex Gravem <155854700+recoeur@users.noreply.github.com>
2024-05-09 16:10:32 +0100

    feat: forward http headers (#8)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod providers;
 
 use bytes::Bytes;
 use http::header::HOST;
-use http::{HeaderValue, StatusCode};
+use http::{HeaderName, StatusCode};
 use http_body_util::combinators::BoxBody;
 use http_body_util::{BodyExt, Empty};
 use hyper::client::conn::http1 as client;
@@ -14,7 +14,6 @@ use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 use miette::Result;
 use std::net::SocketAddr;
-use std::str::FromStr;
 use std::sync::Arc;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::mpsc::Sender;
@@ -76,20 +75,28 @@ async fn proxy(
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
     debug!(method = %req.method(), uri = %req.uri(), "Request");
 
+    let method = req.method().clone();
+    let headers = req.headers().clone();
     let (parts, body) = req.into_parts();
-    let host = parts.headers.get(HOST).unwrap().to_str().unwrap();
-    let port = 80;
+    let host = parts
+        .headers
+        .get(HOST)
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
 
     match provider.get(host) {
         Some(endpoint) => {
             debug!("Matched endpont: {}", endpoint.hostname);
             let backend = endpoint.get_backend(parts.uri.to_string()).unwrap();
+
             debug!("Forwarding request to: {}", backend.location);
-            let addr = format!("{}:{}", backend.location, port);
+            let (host, port) = parse_host(backend.location.as_str());
+            let addr = format!("{}:{}", host, port);
+            debug!("Connecting to: {}", addr);
             let stream = TcpStream::connect(addr).await.unwrap();
-
             let io = TokioIo::new(stream);
-
             let (mut sender, conn) = client::Builder::new().handshake(io).await.unwrap();
 
             tokio::task::spawn(async move {
@@ -97,13 +104,14 @@ async fn proxy(
             });
 
             let host = backend.location.as_str();
+            let mut req = Request::builder().method(method).header(HOST, host);
+            let mut_headers = req.headers_mut().unwrap();
 
-            let req = Request::builder()
-                .method("GET")
-                .header(HOST, host)
-                .body(body)
-                .unwrap();
+            for (name, value) in headers.iter() {
+                mut_headers.insert(name, value.to_owned());
+            }
 
+            let req = req.body(body).unwrap();
             let uri = req.uri().to_string();
             let res = sender.send_request(req).await.unwrap().map(|r| r.boxed());
 
@@ -128,4 +136,15 @@ fn empty() -> BoxBody<Bytes, hyper::Error> {
     Empty::<Bytes>::new()
         .map_err(|never| match never {})
         .boxed()
+}
+
+fn parse_host(host: &str) -> (String, u16) {
+    let parts: Vec<&str> = host.split(':').collect();
+    let host = parts[0].to_string();
+    let port = match parts.get(1) {
+        Some(part) => part.parse::<u16>().unwrap_or(80),
+        None => 80,
+    };
+
+    (host, port)
 }
