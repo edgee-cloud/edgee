7baf15430a20b4d66f5ff8813f412f77ed614f83
Alex Gravem <a@edgee.cloud>
2024-07-29 22:21:27 +0100

    chore(ci): testing multi-architecture build

--- flake.nix
+++ flake.nix
@@ -3,35 +3,25 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    rust-overlay.url = "https://flakehub.com/f/oxalica/rust-overlay/*.tar.gz";
+    flake-utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.92.tar.gz";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-  }: let
-    overlays = [
-      rust-overlay.overlays.default
-      (final: prev: {
-        rustToolchain = prev.rust-bin.stable.latest.default.override {
-          extensions = ["rust-src" "llvm-tools"];
-        };
-      })
-    ];
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f { pkgs = import nixpkgs {inherit overlays system;}; });
-  in {
-    packages = forAllSystems ({ pkgs }: {
-      default = pkgs.rustPlatform.buildRustPackage {
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }: 
+   flake-utils.lib.eachDefaultSystem(system:
+    let 
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      rust-pkg =  (pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" "rustfmt" "llvm-tools" ];
+        targets = [ "aarch64-apple-darwin" "x86_64-apple-darwin" "aarch64-unknown-linux-gnu" "x86_64-unknown-linux-gnu" ];
+      });
+    in 
+    {
+      packages.default = with pkgs; rustPlatform.buildRustPackage {
         pname = "edgee";
         version = "0.2.0";
-        buildInputs = with pkgs; [ 
-          rustToolchain 
-        ] ++ lib.optionals stdenv.isDarwin [
+        buildInputs = [ rust-pkg ] ++ lib.optionals stdenv.isDarwin [
           libiconv
           darwin.apple_sdk.frameworks.Security
           darwin.apple_sdk.frameworks.SystemConfiguration
@@ -39,26 +29,13 @@
         src = ./.;
         cargoLock = { lockFile = ./Cargo.lock; };
       };
-    });
 
-    devShells = forAllSystems ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          rustToolchain
-          cargo-deny
-          cargo-edit
-          cargo-expand
-          cargo-watch
-          rust-analyzer
-          rustfmt
-
-          gh
-        ] ++ lib.optionals stdenv.isDarwin [
+    devShells.default = with pkgs; mkShell {
+        packages = [ rust-pkg ] ++ lib.optionals stdenv.isDarwin [
           libiconv
           darwin.apple_sdk.frameworks.Security
           darwin.apple_sdk.frameworks.SystemConfiguration
         ];
       };
     });
-  };
 }
