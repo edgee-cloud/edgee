63b6d85185f2b9b38986cb36c1a6389e6b90a7a1
Julianne H. <julianne@edgee.cloud>
2024-09-27 15:53:16 +0200

    feat: Allow Specifying a Config File Path on Execution
    - Also add CLI options parsing base

--- src/config/config.rs
+++ src/config/config.rs
@@ -201,11 +201,33 @@ fn default_max_compressed_body_size() -> usize {
     3000000
 }
 
-fn read_config() -> Result<StaticConfiguration, String> {
-    let toml_exists = Path::new("edgee.toml").exists();
-    let yaml_exists = Path::new("edgee.yaml").exists();
+fn read_config(path: Option<&Path>) -> Result<StaticConfiguration, String> {
+    let toml_path = Path::new("edgee.toml");
+    let yaml_path = Path::new("edgee.yaml");
 
-    match (toml_exists, yaml_exists) {
+    if let Some(path) = path {
+        let extension = path
+            .extension()
+            .and_then(|extension| extension.to_str())
+            .expect("provided configuration file does not have a format extension or is invalid");
+
+        let config_data =
+            std::fs::read_to_string(path).expect("should read provided configuration file");
+
+        match extension {
+            "toml" => {
+                return toml::from_str(&config_data)
+                    .map_err(|e| format!("should parse config file: {e}"))
+            }
+            "yml" | "yaml" => {
+                return serde_yml::from_str(&config_data)
+                    .map_err(|e| format!("should parse config file: {e}"));
+            }
+            _ => return Err("provided configuration file has an unknown extension".to_string()),
+        }
+    }
+
+    match (toml_path.exists(), yaml_path.exists()) {
         (true, true) => {
             Err("both edgee.toml and edgee.yaml exist but only one is expected.".into())
         }
@@ -213,24 +235,21 @@ fn read_config() -> Result<StaticConfiguration, String> {
             Err("no configuration file found, either edgee.toml or edgee.yaml is required.".into())
         }
         (true, false) => {
-            let config_file =
-                std::fs::read_to_string("edgee.toml").expect("should read edgee.toml");
+            let config_file = std::fs::read_to_string(toml_path).expect("should read edgee.toml");
             toml::from_str(&config_file).map_err(|e| format!("should parse config file: {}", e))
         }
         (false, true) => {
-            let config_file =
-                std::fs::read_to_string("edgee.yaml").expect("should read edgee.yaml");
+            let config_file = std::fs::read_to_string(yaml_path).expect("should read edgee.yaml");
             serde_yml::from_str(&config_file)
                 .map_err(|e| format!("should parse config file: {}", e))
         }
     }
 }
 
-// TODO: Read config from CLI arguments
 // TODO: Add more configuration validations
 // TODO: Improve error messages for configuration errors
-pub fn init() {
-    let config = read_config().expect("should read config file");
+pub fn init(path: Option<&Path>) {
+    let config = read_config(path).expect("should read config file");
     config.validate().unwrap();
 
     CONFIG.set(config).expect("Should initialize config");
