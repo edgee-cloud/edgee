8ab67a5d605aa9e10893536ab3319b7db7020891
Julianne H. <julianne@edgee.cloud>
2024-10-15 12:35:52 +0200

    feat(logger): Add ability to specify span to log filter in config
    - Also add logging filtering directives from env variables if present

--- src/logger/logger.rs
+++ src/logger/logger.rs
@@ -1,20 +1,68 @@
-use crate::config::config;
-use tracing_subscriber::prelude::*;
-use tracing_subscriber::{fmt, registry, EnvFilter};
+/// Define log format used
+///
+/// Defaults to `Basic` when building in debug profile, `Json` when building in release profile
+#[derive(Debug, Clone, Copy, Default, clap::ValueEnum)]
+pub enum LogFormat {
+    #[cfg_attr(debug_assertions, default)]
+    Basic,
+    Pretty,
+    #[cfg_attr(not(debug_assertions), default)]
+    Json,
+}
 
-const ACCEPTED_LEVELS: [&str; 6] = ["trace", "debug", "info", "warn", "error", "fatal"];
+/// Initializing logging facilities
+///
+/// Log filtering is configured with env var and config in this priority order:
+/// - `EDGEE_LOG` env variable, formatted for [tracing_subscriber::EnvFilter]
+/// - `RUST_LOG` "standard" env variable, also formatted for [tracing_subscriber::EnvFilter]
+/// - Config file, specifying level and optionally span
+///
+/// In the case something goes wrong with parsing of these directives, logging is done
+/// using the log level defined in config
+pub fn init(log_format: LogFormat) {
+    use std::env;
 
-pub fn init() {
-    let level = &config::get().log.level;
+    use tracing_subscriber::prelude::*;
+    use tracing_subscriber::{fmt, EnvFilter};
 
-    if !ACCEPTED_LEVELS.contains(&level.as_str()) {
-        panic!("Unsupported log level: {level}");
-    }
+    use crate::config::config;
 
-    let filter: EnvFilter = level.into();
-    if cfg!(debug_assertions) {
-        registry().with(fmt::layer()).with(filter).init();
-    } else {
-        registry().with(fmt::layer().json()).with(filter).init();
-    }
+    let config = &config::get().log;
+
+    let fmt_layer = fmt::layer();
+    let fmt_layer = match log_format {
+        LogFormat::Basic | LogFormat::Pretty => fmt_layer.boxed(),
+        LogFormat::Json => fmt_layer.json().boxed(),
+    };
+
+    let filter_layer = {
+        let builder = EnvFilter::builder().with_default_directive(config.level.into());
+
+        // Get logging directives from EDGEE_LOG or standard RUST_LOG env variables
+        let directives = env::var("EDGEE_LOG")
+            .or_else(|_| env::var("RUST_LOG"))
+            .unwrap_or_else(|_| {
+                let mut directive = config.level.to_string();
+
+                if let Some(ref span) = config.span {
+                    directive = format!("{directive}[{span}]");
+                }
+
+                directive
+            });
+
+        builder.parse_lossy(directives)
+    };
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
+
+    // let filter: EnvFilter = EnvFilter::from(&config.level);
+    // if cfg!(debug_assertions) {
+    //     registry().with(fmt::layer()).with(filter).init();
+    // } else {
+    //     registry().with(fmt::layer().json()).with(filter).init();
+    // }
 }
