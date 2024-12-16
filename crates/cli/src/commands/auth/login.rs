539137a035d90d157bcd09a31a1ca7bf351a5789
Julianne H. <julianne@edgee.cloud>
2024-12-16 12:27:07 +0100

    refactor(cli/auth): Change URL on login for creating a token

--- crates/cli/src/commands/auth/login.rs
+++ crates/cli/src/commands/auth/login.rs
@@ -15,9 +15,7 @@ pub async fn run(_opts: Options) {
     }
 
     let api_token = Password::new("Enter Edgee API token (press Ctrl+R to toggle input display):")
-        .with_help_message(
-            "You can create one at https://www.edgee.cloud/<username>/settings/tokens",
-        )
+        .with_help_message("You can create one at https://www.edgee.cloud/me/settings/tokens")
         .with_display_mode(PasswordDisplayMode::Masked)
         .with_display_toggle_enabled()
         .without_confirmation()
