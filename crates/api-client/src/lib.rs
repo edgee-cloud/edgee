24376a3d064e0be5a524ae6437d880b1b06dbfc9
Julianne H. <julianne@edgee.cloud>
2025-01-10 16:24:07 +0100

    refactor(api-client): Add helpers for instantiate a fully-configured client

--- crates/api-client/src/lib.rs
+++ crates/api-client/src/lib.rs
@@ -6,8 +6,27 @@ progenitor::generate_api! {
 
 pub const PROD_BASEURL: &str = "https://api.edgee.app";
 
-impl Client {
-    pub fn new_prod() -> Self {
-        Self::new(PROD_BASEURL)
-    }
+/// This crate's entry point
+///
+/// Use this function to build a client configured to interact with Edgee API using provided
+/// credentials
+#[bon::builder(
+    start_fn = new,
+    finish_fn = connect,
+    on(String, into),
+)]
+pub fn connect(#[builder(default = PROD_BASEURL)] baseurl: &str, api_token: String) -> Client {
+    use reqwest::header::{self, HeaderMap};
+
+    let mut default_headers = HeaderMap::new();
+
+    let auth_value = format!("Bearer {api_token}");
+    default_headers.insert(header::AUTHORIZATION, auth_value.try_into().unwrap());
+
+    let client = reqwest::Client::builder()
+        .default_headers(default_headers)
+        .build()
+        .unwrap();
+
+    Client::new_with_client(baseurl, client)
 }
