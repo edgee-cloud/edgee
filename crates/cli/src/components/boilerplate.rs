d7d297015e980f9294957bfef09af7afbd996b9d
Nicolas Girardot <nicolas.girardot+github@gmail.com>
2025-02-18 16:45:22 +0100

    feat: add option to oneshot the new command

--- crates/cli/src/components/boilerplate.rs
+++ crates/cli/src/components/boilerplate.rs
@@ -3,6 +3,7 @@ pub struct LanguageConfig {
     pub name: &'static str,
     pub repo_url: &'static str,
     pub default_build_command: &'static str,
+    pub alias: &'static [&'static str],
 }
 
 impl std::fmt::Display for LanguageConfig {
@@ -40,36 +41,43 @@ pub static LANGUAGE_OPTIONS: &[LanguageConfig] = &[
         name: "C",
         repo_url: "https://github.com/edgee-cloud/example-c-component",
         default_build_command: "gcc main.c -o main",
+        alias: &["c"],
     },
     LanguageConfig {
         name: "CSharp",
         repo_url: "https://github.com/edgee-cloud/example-csharp-component",
         default_build_command: "dotnet build",
+        alias: &["csharp", "cs", "c#"],
     },
     LanguageConfig {
         name: "Go",
         repo_url: "https://github.com/edgee-cloud/example-go-component",
         default_build_command: "go build -o main .",
+        alias: &["go", "golang"],
     },
     LanguageConfig {
         name: "JavaScript",
         repo_url: "https://github.com/edgee-cloud/example-js-component",
         default_build_command: "node main.js",
+        alias: &["js", "javascript"],
     },
     LanguageConfig {
         name: "Python",
         repo_url: "https://github.com/edgee-cloud/example-py-component",
         default_build_command: "python main.py",
+        alias: &["py", "python"],
     },
     LanguageConfig {
         name: "Rust",
         repo_url: "https://github.com/edgee-cloud/example-rust-component",
         default_build_command: "cargo build --release",
+        alias: &["rs", "rust"],
     },
     LanguageConfig {
         name: "TypeScript",
         repo_url: "https://github.com/edgee-cloud/example-ts-component",
         default_build_command: "npx tsc",
+        alias: &["ts", "typescript"],
     },
 ];
 
