8e844c5452905961f435f82ae849a1cd8422f14a
Julianne H. <julianne@edgee.cloud>
2025-03-21 19:06:18 +0100

    feat(cli/telemetry): add bunch of metadata and fixes to telemetry

--- crates/cli/src/telemetry.rs
+++ crates/cli/src/telemetry.rs
@@ -1,11 +1,21 @@
 use std::future::Future;
+use std::path::PathBuf;
+use std::sync::LazyLock;
 use std::{collections::HashMap, time::Duration};
 
 use anyhow::Result;
 
+use edgee_api_client::types::UserWithRoles;
 use event_builder::{IsComplete, IsUnset, SetProperties, State};
+use serde::{Deserialize, Serialize};
 
-const TELEMETRY_BASE_URL: &str = "https://edgee-cli.edgee.team";
+static STATE_DIR: LazyLock<Option<PathBuf>> = LazyLock::new(|| {
+    dirs::state_dir()
+        .or_else(dirs::config_dir)
+        .map(|path| path.join("edgee"))
+});
+
+const TELEMETRY_BASE_URL: &str = "https://edgee-cli.edgee.app";
 const TELEMETRY_TIMEOUT: Duration = Duration::from_millis(1000);
 const TELEMETRY_WARNING: &str = r#"Welcome to the Edgee CLI!
 
@@ -16,21 +26,70 @@ You can opt-out of telemetry by setting the EDGEE_TELEMETRY_OPTOUT environment
 variable to '1' or 'true' using your favorite shell.
 "#;
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Data {
+    is_logged_in: bool,
+    id: String,
+}
+
+impl Data {
+    const FILENAME: &str = "telemetry.json";
+
+    fn new() -> Self {
+        Data {
+            is_logged_in: false,
+            id: uuid::Uuid::new_v4().to_string(),
+        }
+    }
+
+    fn load() -> Result<Self> {
+        let data_file = STATE_DIR
+            .as_ref()
+            .ok_or_else(|| anyhow::anyhow!("no state dir"))?
+            .join(Self::FILENAME);
+        let f = std::fs::File::open(data_file)?;
+        serde_json::from_reader(f).map_err(Into::into)
+    }
+}
+
 pub fn setup() -> Result<()> {
-    let state_dir = dirs::state_dir()
-        .or_else(dirs::config_dir)
-        .ok_or_else(|| anyhow::anyhow!("state dir is not existent"))?
-        .join("edgee");
-    std::fs::create_dir_all(&state_dir)?;
+    let state_dir = STATE_DIR
+        .as_ref()
+        .ok_or_else(|| anyhow::anyhow!("state dir is not existent"))?;
+    std::fs::create_dir_all(state_dir)?;
 
-    let first_run = state_dir.join("first_run.marker");
-    if !first_run.exists() {
-        eprintln!("{TELEMETRY_WARNING}");
+    let data_file = state_dir.join(Data::FILENAME);
+    if !data_file.exists() {
+        // If data file is not here it means it's first run
+        println!("{TELEMETRY_WARNING}");
 
-        drop(std::fs::File::create_new(&first_run)?);
+        let f = std::fs::File::create_new(&data_file)?;
+        serde_json::to_writer(f, &Data::new())?;
     }
 
     Ok(())
+}
+
+pub fn login(user: &UserWithRoles) -> Result<()> {
+    let data = Data {
+        is_logged_in: true,
+        id: user.id.clone(),
+    };
+
+    let data_file = STATE_DIR
+        .as_ref()
+        .ok_or_else(|| anyhow::anyhow!("no state dir"))?
+        .join(Data::FILENAME);
+    let f = std::fs::File::create(data_file)?;
+
+    serde_json::to_writer(f, &data).map_err(Into::into)
+}
+
+pub fn is_telemetry_enabled() -> bool {
+    let Ok(value) = std::env::var("EDGEE_TELEMETRY_OPTOUT") else {
+        return true;
+    };
+    value != "1" && value != "true"
 }
 
 pub async fn process_cli_command<F, T, E>(f: F) -> Result<T, E>
@@ -94,15 +153,18 @@ impl Event {
     pub async fn send(self) -> Result<()> {
         use edgee_api_client::data_collection as dc;
 
-        if let Ok(value) = std::env::var("EDGEE_TELEMETRY_OPTOUT") {
-            if value == "1" || value == "true" {
-                return Ok(());
-            }
+        if !is_telemetry_enabled() {
+            return Ok(());
         }
 
+        let data = Data::load()?;
+
         let client = dc::new()
-            .baseurl(TELEMETRY_BASE_URL)
-            .debug_mode(true)
+            .baseurl(
+                std::env::var("EDGEE_TELEMETRY_BASEURL")
+                    .unwrap_or_else(|_| TELEMETRY_BASE_URL.to_string()),
+            )
+            .debug_mode(std::env::var("EDGEE_TELEMETRY_DEBUG").is_ok_and(|value| value == "1"))
             .with_client_builder(|builder| builder.timeout(TELEMETRY_TIMEOUT))
             .connect();
 
@@ -117,8 +179,15 @@ impl Event {
             .title(self.title)
             .url("cli://edgee-cli")
             .path("/");
-        let context =
-            dc::types::EdgeeEventDataCollectionContext::builder().page(Some(page.try_into()?));
+        let user = dc::types::EdgeeEventUserData::builder();
+        let user = if data.is_logged_in {
+            user.user_id(Some(data.id))
+        } else {
+            user.anonymous_id(Some(data.id))
+        };
+        let context = dc::types::EdgeeEventDataCollectionContext::builder()
+            .page(Some(page.try_into()?))
+            .user(Some(user.try_into()?));
 
         let payload = dc::types::EdgeeEvent::builder().data_collection(
             dc::types::EdgeeEventDataCollection::builder()
@@ -132,7 +201,7 @@ impl Event {
             .send()
             .await
             .inspect_err(|err| {
-                dbg!(err);
+                tracing::debug!("Telemetry error: {err}");
             })?;
 
         Ok(())
