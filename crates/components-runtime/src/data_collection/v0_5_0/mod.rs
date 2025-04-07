f13c27c29a7af6b824d16585f227fd411e099884
Clement Bouvet <clement@cbouvet.fr>
2025-04-07 10:11:28 +0200

    cleanup

--- crates/components-runtime/src/data_collection/v0_5_0/mod.rs
+++
@@ -1,9 +0,0 @@
-mod convert;
-
-pub mod data_collection {
-    wasmtime::component::bindgen!({
-        world: "data-collection-zero-five-zero",
-        path: "wit/",
-        async: true,
-    });
-}
