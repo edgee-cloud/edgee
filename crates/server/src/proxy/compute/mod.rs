a38911d246cd6c6ec5ca58abb271c98d534147d6
Sacha Morard <2254275+SachaMorard@users.noreply.github.com>
2025-04-28 09:29:32 +0200

    feat(security): add 'empty' to forbidden fetch destinations

--- crates/server/src/proxy/compute/mod.rs
+++ crates/server/src/proxy/compute/mod.rs
@@ -132,7 +132,7 @@ fn do_process_payload(request: &RequestHandle, response: &mut Parts) -> Result<b
         .unwrap_or("".to_string());
     if !set_fetch_dest.is_empty() {
         let forbidden_fetch_dest = [
-            "audio", "font", "image", "manifest", "script", "style", "video",
+            "audio", "font", "image", "manifest", "script", "style", "video", "empty",
         ];
         if forbidden_fetch_dest.contains(&set_fetch_dest.as_str()) {
             Err("compute-aborted(fetch-dest)")?;
