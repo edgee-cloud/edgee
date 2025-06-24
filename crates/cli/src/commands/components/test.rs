7d66872faa7f50a1916f4939052b81c0067cf19e
Clément B <clement@edgee.cloud>
2025-06-24 10:09:19 +0200

    test: patch events with current timestamp (#350)

--- crates/cli/src/commands/components/test.rs
+++ crates/cli/src/commands/components/test.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use colored::Colorize;
 use colored_json::prelude::*;
 use edgee_components_runtime::config::{ComponentsConfiguration, DataCollectionComponents};
@@ -134,6 +135,16 @@ async fn test_data_collection_component(opts: Options, manifest: &Manifest) -> a
     let page_event_json = r#"[{"uuid":"37009b9b-a572-4615-87c1-09e257331ecb","timestamp":"2025-02-03T15:46:39.283317613Z","type":"page","data":{"keywords":["demo","tag manager"],"title":"Page with Edgee components","url":"https://demo.edgee.app/analytics-with-edgee.html","path":"/analytics-with-edgee.html","referrer":"https://demo.edgee.dev/analytics-with-js.html"},"context":{"page":{"keywords":["demo","tag manager"],"title":"Page with Edgee components","url":"https://demo.edgee.app/analytics-with-edgee.html","path":"/analytics-with-edgee.html","referrer":"https://demo.edgee.dev/analytics-with-js.html"},"user":{"edgee_id":"6bb171d5-2284-41ee-9889-91af03b71dc5"},"client":{"ip":"127.0.0.1","locale":"en-us","accept_language":"en-US,en;q=0.9","timezone":"Europe/Paris","user_agent":"Mozilla/5.0 (X11; Linux x86_64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36","user_agent_version_list":"Not A(Brand;8|Chromium;132","user_agent_mobile":"0","os_name":"Linux","user_agent_architecture":"x86","user_agent_bitness":"64","user_agent_full_version_list":"Not A(Brand;8.0.0.0|Chromium;132.0.6834.159","user_agent_model":"","os_version":"6.12.11","screen_width":1920,"screen_height":1280,"screen_density":1.5},"session":{"session_id":"1738597536","session_count":1,"session_start":false,"first_seen":"2025-02-03T15:45:36.569004889Z","last_seen":"2025-02-03T15:46:39.278740029Z"}},"from":"edge"}]"#;
     let track_event_json = r#" [{"uuid":"4cffe10b-b5fd-429e-96d2-471f0575005f","timestamp":"2025-02-03T16:06:32.809486270Z","type":"track","data":{"name":"button_click","properties":{"registered":false,"size":10,"color":"blue","category":"shoes","label":"Blue Sneakers"}},"context":{"page":{"keywords":["demo","tag manager"],"title":"Page with Edgee components","url":"https://demo.edgee.app/analytics-with-edgee.html","path":"/analytics-with-edgee.html","referrer":"https://demo.edgee.dev/"},"user":{"user_id":"123456","anonymous_id":"anon-123","edgee_id":"69659401-40cf-4ac8-8ffc-630a10fe06dc","properties":{"verified":true,"age":42,"email":"me@example.com","name":"John Doe"}},"client":{"ip":"127.0.0.1","locale":"en-us","accept_language":"en-US,en;q=0.5","timezone":"Europe/Paris","user_agent":"Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0","screen_width":1440,"screen_height":960,"screen_density":2.0},"session":{"session_id":"1738598699","session_count":7,"session_start":false,"first_seen":"2024-12-12T16:30:03.693248190Z","last_seen":"2025-02-03T16:06:32.808844970Z"}},"from":"client","consent":"granted"}]"#;
     let user_event_json = r#"[{"uuid":"eb0f001a-cd2b-42c4-9c71-7b1c2bcda445","timestamp":"2025-02-03T16:07:04.878715197Z","type":"user","data":{"user_id":"123456","anonymous_id":"anon-123","edgee_id":"69659401-40cf-4ac8-8ffc-630a10fe06dc","properties":{"age":42,"verified":true,"name":"John Doe","email":"me@example.com"}},"context":{"page":{"keywords":["demo","tag manager"],"title":"Page with Edgee components","url":"https://demo.edgee.app/analytics-with-edgee.html","path":"/analytics-with-edgee.html","referrer":"https://demo.edgee.dev/"},"user":{"user_id":"123456","anonymous_id":"anon-123","edgee_id":"69659401-40cf-4ac8-8ffc-630a10fe06dc","properties":{"email":"me@example.com","age":42,"name":"John Doe","verified":true}},"client":{"ip":"127.0.0.1","locale":"en-us","accept_language":"en-US,en;q=0.5","timezone":"Europe/Paris","user_agent":"Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0","screen_width":1440,"screen_height":960,"screen_density":2.0},"session":{"session_id":"1738598699","session_count":7,"session_start":false,"first_seen":"2024-12-12T16:30:03.693248190Z","last_seen":"2025-02-03T16:07:04.878137016Z"}},"from":"client","consent":"granted"}]"#;
+    //
+    let mut page_event = serde_json::from_str::<Vec<Event>>(page_event_json).unwrap()[0].clone();
+    let mut track_event = serde_json::from_str::<Vec<Event>>(track_event_json).unwrap()[0].clone();
+    let mut user_event = serde_json::from_str::<Vec<Event>>(user_event_json).unwrap()[0].clone();
+
+    // set the timestamp to now
+    page_event.timestamp = Utc::now();
+    track_event.timestamp = Utc::now();
+    user_event.timestamp = Utc::now();
+
     // read the event file if provided
 
     let custom_event_data = match opts.events_file {
@@ -208,30 +219,21 @@ async fn test_data_collection_component(opts: Options, manifest: &Manifest) -> a
                 }
                 None => {
                     // use default events if no custom data is provided
-                    events.push(
-                        serde_json::from_str::<Vec<Event>>(page_event_json).unwrap()[0].clone(),
-                    );
-                    events.push(
-                        serde_json::from_str::<Vec<Event>>(track_event_json).unwrap()[0].clone(),
-                    );
-                    events.push(
-                        serde_json::from_str::<Vec<Event>>(user_event_json).unwrap()[0].clone(),
-                    );
+                    events.push(page_event);
+                    events.push(track_event);
+                    events.push(user_event);
                 }
             }
         }
         Some(event_type) => match event_type.as_str() {
             "page" => {
-                events
-                    .push(serde_json::from_str::<Vec<Event>>(page_event_json).unwrap()[0].clone());
+                events.push(page_event);
             }
             "track" => {
-                events
-                    .push(serde_json::from_str::<Vec<Event>>(track_event_json).unwrap()[0].clone());
+                events.push(track_event);
             }
             "user" => {
-                events
-                    .push(serde_json::from_str::<Vec<Event>>(user_event_json).unwrap()[0].clone());
+                events.push(user_event);
             }
             _ => {
                 return Err(anyhow::anyhow!("Invalid event type"));
